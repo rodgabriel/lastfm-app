8e2c8cbf13e311882fe5bb0901456ba1986a18f3
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-02-05 14:33:39 -0300

    Recebe os top 10 artistas do backend

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// css classes
 import {
   wrapper,
   navbar,
@@ -7,6 +11,23 @@ import {
 } from "./styles.module.scss";
 
 const HomePage = () => {
+  const [topArtists, setTopArtists] = useState([]);
+
+  useEffect(() => {
+    async function getTopArtists() {
+      const { data } = await axios.post("http://localhost:5000/top", {
+        params: {
+          method: "geo.gettopartists",
+          country: "brazil",
+        },
+      });
+      setTopArtists(data["topartists"]["artist"]);
+    }
+
+    getTopArtists();
+  }, []);
+
+  console.log(topArtists);
   return (
     <div className={wrapper}>
       <nav className={navbar}>
