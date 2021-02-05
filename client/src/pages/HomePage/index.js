7c81a829317b2d58d6c92970d188edd1eb29a3ef
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-02-05 17:25:47 -0300

    Mostra os Top 10 artistas

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -8,6 +8,14 @@ import {
   userSection,
   userImg,
   logBtn,
+  main,
+  cardSection,
+  cardContainer,
+  card,
+  cardHeader,
+  cardContent,
+  cardAvatar,
+  searchSection,
 } from "./styles.module.scss";
 
 const HomePage = () => {
@@ -21,13 +29,14 @@ const HomePage = () => {
           country: "brazil",
         },
       });
-      setTopArtists(data["topartists"]["artist"]);
+      setTopArtists(data);
     }
 
     getTopArtists();
   }, []);
 
   console.log(topArtists);
+
   return (
     <div className={wrapper}>
       <nav className={navbar}>
@@ -37,6 +46,31 @@ const HomePage = () => {
           <div className={logBtn}>SIGN IN | OUT</div>
         </div>
       </nav>
+      <main className={main}>
+        <section className={searchSection}>Search section</section>
+        <section className={cardSection}>
+          <h1>Top 10 Artistas no Brasil</h1>
+          <div className={cardContainer}>
+            {topArtists &&
+              topArtists.map((artist) => {
+                return (
+                  <div className={card}>
+                    <div className={cardHeader}>
+                      <div className={cardAvatar}>
+                        <p>{artist.name.slice(0, 1)}</p>
+                      </div>
+                    </div>
+                    <div className={cardContent}>
+                      <h2>{artist.name}</h2>
+                      <p>{artist.listeners} ouvintes mensais</p>
+                      <p>Saiba mais!</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
