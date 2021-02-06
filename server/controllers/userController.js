6b086061eedbdb83ea7d5a3be29823f3c651c9d4
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-02-06 15:14:26 -0300

    Rota para cadastrar usuario

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,48 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
-// Auth user POST/users/login
+// Auth user POST /users/login
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.send(email, password);
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status();
+    throw new Error("Email ou senha inválidos.");
+  }
 });
 
-export { authUser };
+// Registrar user POST /users
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email já cadastrado.");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Informação de usuário inválida");
+  }
+});
+
+export { authUser, registerUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
+
+router.route("/").post(registerUser);
 
 router.post("/login", authUser);
 
