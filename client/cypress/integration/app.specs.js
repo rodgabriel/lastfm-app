518211533824245ab1675706a14a6a6ac22607e1
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-02-08 11:42:48 -0300

    Adiciona teste de pesquisas

--- client/cypress/integration/app.specs.js
+++ client/cypress/integration/app.specs.js
@@ -3,11 +3,11 @@ describe("Cypress", () => {
     expect(true).to.equal(true);
   });
 
-  it("limpa o banco de dados para poder realizar os testes seguintes conforme esperado", () => {
+  it("Limpa o banco de dados para poder realizar os testes seguintes conforme esperado", () => {
     cy.request("POST", "http://localhost:5000/clearDatabase");
   });
 
-  it("testa busca por artista", () => {
+  it("Usuário não-logado faz busca por artista", () => {
     cy.visit("/");
 
     cy.contains("Artista").click();
@@ -17,7 +17,7 @@ describe("Cypress", () => {
     cy.url().should("include", "/artist=Coldplay");
   });
 
-  it("testa busca por album", () => {
+  it("Usuário não-logado faz busca por album", () => {
     cy.visit("/");
 
     cy.contains("Álbum").click();
@@ -28,7 +28,7 @@ describe("Cypress", () => {
     cy.url().should("include", "/artist=Coldplay/album=Parachutes");
   });
 
-  it("testa registro de usuario", () => {
+  it("Faz registro de usuario", () => {
     cy.visit("/");
 
     cy.get("button[data-cy=login]").click();
@@ -46,7 +46,7 @@ describe("Cypress", () => {
     cy.url().should("eq", "http://localhost:3000/");
   });
 
-  it("testa login e logout do usuário", () => {
+  it("Usuário faz login e logout", () => {
     cy.visit("/");
 
     cy.get("button[data-cy=login]").click();
@@ -63,7 +63,7 @@ describe("Cypress", () => {
     cy.get("button[data-cy=login]");
   });
 
-  it("testa mensagem de erro ao tentar logar com infos incorretas", () => {
+  it("Mostra mensagem de erro ao usuário ao tentar logar com infos incorretas", () => {
     cy.visit("/");
 
     cy.get("button[data-cy=login]").click();
@@ -75,7 +75,7 @@ describe("Cypress", () => {
     cy.contains("Email ou senha inválidos.");
   });
 
-  it("testa mensagem de erro caso email já esteja cadastrado", () => {
+  it("Mostra mensagem de erro quando o usuário tenta registrar um email já esteja cadastrado", () => {
     cy.visit("/");
 
     cy.get("button[data-cy=login]").click();
@@ -91,5 +91,32 @@ describe("Cypress", () => {
     cy.get("input[data-cy=confirmPassword]").type("12345");
     cy.get("button[data-cy=registrar]").click();
     cy.contains("Email já cadastrado.");
+  });
+
+  it("Usuário faz login, faz buscas e consulta seu histórico de pesquisas", () => {
+    cy.visit("/");
+
+    cy.get("button[data-cy=login]").click();
+
+    cy.url().should("include", "/login");
+
+    cy.get("input[data-cy=email]").type("joao@email.com");
+    cy.get("input[data-cy=password]").type("12345").type("{enter}");
+    cy.url().should("eq", "http://localhost:3000/");
+
+    // Pesquisa o artista Coldplay
+    cy.visit("/");
+    cy.contains("Artista").click();
+    cy.get("input[data-cy=artist]").type("Coldplay").type("{enter}");
+    cy.url().should("include", "/artist=Coldplay");
+
+    // Pesquisa a artista Adele
+    cy.visit("/");
+    cy.contains("Artista").click();
+    cy.get("input[data-cy=artist]").type("Adele").type("{enter}");
+    cy.url().should("include", "/artist=Adele");
+
+    // Checa página do Histórico de Pesquisas
+    cy.visit("/pesquisas");
   });
 });
